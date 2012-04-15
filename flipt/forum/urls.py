26223bd0f4764ed9c1ae33d52d95e8caa4f2f6a7
Anthony <anthonyarr@gmail.com>
2012-04-14 23:57:47 -0500

    fixed previous error, now it's throwing an index ewrror when I try to save

--- flipt/forum/urls.py
+++ flipt/forum/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 from flipt.forum.models import *
 
-urlpatterns = patterns('forum.views',
+urlpatterns = patterns('flipt.forum.views',
     (r"^forum/(\d+)/$", "forum"),
     (r"^thread/(\d+)/$", "thread"),
     (r"^post/(new_thread|reply)/(\d+)/$", "post"),
